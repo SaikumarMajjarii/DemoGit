d087c3aabdf9c987c7788e198d50d334fe385f63
siddu <saikumar.majjarii@gmail.com>
2022-05-12 00:45:48 +0530

    Commited by American Guy

--- src/Product.java
+++ src/Product.java
@@ -18,6 +18,9 @@ public class Product {
 		System.out.println("this is Git X Change 1");
 		
 		System.out.println("this is Git X writing Code ");
+		
+		System.out.println("this is Git y Change 2");
+		System.out.println("this is Git y writing Code ");
 		// TODO Auto-generated method stub
 
 	}
