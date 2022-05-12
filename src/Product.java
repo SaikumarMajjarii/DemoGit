ed0e81bdbc76a65a8598b7a523d7f9e9a38105a1
siddu <saikumar.majjarii@gmail.com>
2022-05-12 16:33:00 +0530

    Changes done by Devbranch Team

--- src/Product.java
+++ src/Product.java
@@ -5,6 +5,14 @@ public class Product {
 	public void DisplayDetails()
 	{
 		
+		System.out.println("this is Git X Change 1");
+		
+		System.out.println("this is Git X writing Code ");
+		
+		System.out.println("this is Git y Change 2");
+		System.out.println("this is Git y writing Code ");
+		// TODO Auto-generated method stub
+
 	
 	}
 	
